744dd3d4fab460e79de818182f5e68660ff140d4
Clayman <Clayman@DESKTOP-649SK3N>
2020-10-26 17:11:25 +0200

    Simple numbers DONE

--- ControlStructures/FOR_LOOP/main.cpp
+++ ControlStructures/FOR_LOOP/main.cpp
@@ -1,15 +1,31 @@
 #include<iostream>
 using namespace std;
 
+#define FIBONACCI_1
+
 void main()
 {
 	setlocale(LC_ALL, "");
 	int n;
-	cout << "Введите количество итервций: ";
-	cin >> n;
+	cout << "Введите количество чисел Фибоначчи: ";	cin >> n;
+	unsigned long long int a = 0;
+	unsigned long long int b = 1;
+	unsigned long long int c;
+
+	double da = 0;
+	double db = 1;
+	//double dc;
 	for (int i = 0; i < n; i++)
 	{
-		cout << i << "\t";
+		cout << i << " - " << a << "\t\t\t\t";
+		c = a + b;
+		a = b;
+		b = c;
+
+		cout << da << "\n";
+		//dc = da + db;
+		da = db;
+		db = da+db;
 	}
 	cout << endl;
 }
--- ControlStructures/NESTED_FOR/main.cpp
+++ ControlStructures/NESTED_FOR/main.cpp
@@ -1,15 +1,40 @@
 #include<iostream>
+#include<Windows.h>
 using namespace std;
+
+//#define PYTHAGORAS
 
 void main()
 {
 	setlocale(LC_ALL, "");
+
+#ifdef PYTHAGORAS
+	HANDLE hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
+	//SetConsoleTitle(L"Table");
+	//SetConsoleDisplayMode(hConsole, CONSOLE_FULLSCREEN_MODE, 0);
+
 	int n = 10;
+	for (int i = 1; i <= n; i++)
+	{
+		for (int j = 1; j <= n; j++)
+		{
+			cout.width(5);
+			cout /*<< i << "*" << j << "=" */ << i * j << " ";
+		}
+		cout << endl;
+	}
+#endif // PYTHAGORAS
+
+	int n;
+	cout << "Введите число: "; cin >> n;
 	for (int i = 0; i < n; i++)
 	{
-		for (int j = 0; j < n; j++)
+		bool simple = true;	//Предполагаем, что число простое, но это нужно проверить
+		for (int j = 2; j < i; j++)
 		{
-			cout << i << "\t" << j << endl;
+			if (i % j == 0)simple = false;
 		}
+		if(simple)cout << i << "\t";
 	}
+	cout << endl;
 }
