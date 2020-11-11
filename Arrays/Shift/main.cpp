e7b08fc594ba8b9d1404ef2ad6d96a52cc40d06a
Clayman <Clayman@DESKTOP-649SK3N>
2020-11-11 17:01:30 +0200

    Focus Shift

--- Arrays/Shift/main.cpp
+++ Arrays/Shift/main.cpp
@@ -17,4 +17,17 @@ void main()
 		cout << arr[i] << "\t";
 	}
 	cout << endl;
+
+	int buffer = arr[0];
+	for (int i = 0; i < n; i++)
+	{
+		arr[i] = arr[i + 1];
+	}
+	arr[n - 1] = buffer;
+
+	for (int i = 0; i < n; i++)
+	{
+		cout << arr[i] << "\t";
+	}
+	cout << endl;
 }
