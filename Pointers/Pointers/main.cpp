444279e071c42bef867b891091177dd41d160d45
Clayman <Clayman@DESKTOP-DM8E77U>
2021-01-18 17:34:26 +0200

    Pointers day 2

--- Pointers/Pointers/main.cpp
+++ Pointers/Pointers/main.cpp
@@ -1,9 +1,13 @@
 #include<iostream>
 using namespace std;
 
+//#define POINTERS_BASICS
+#define POINTERS_AND_ARRAYS
+
 void main()
 {
 	setlocale(LC_ALL, "Russian");
+#ifdef POINTERS_BASICS
 	int a = 2;
 	int* pa = &a;
 	cout << a << endl;	//Выводим значение переменной 'a' на экран
@@ -12,6 +16,35 @@ void main()
 	cout << *pa << endl;//Разыменовываем адрес в указателе 'pa', и получаем значение по адресу
 
 	int* pb;
-	int b;
-	*pb = &b;
+	int b = 3;
+	pb = &b;
+	//(int*) (int*)
+		/*double price = 2.55;
+		int count = price;
+		cout << count << endl;*/
+#endif // POINTERS_BASICS
+
+#ifdef POINTERS_AND_ARRAYS
+	const int n = 5;
+	int arr[n] = { 3, 5, 8, 13, 21 };
+	cout << arr << endl;
+	cout << *arr << endl;
+#endif // POINTERS_AND_ARRAYS
+
+	//Pointer arithmetics: +, -, ++, --
+
+	/*
+	--------------------
+	char*	- 1Byte;
+	short*	- 2Byte;
+	int*	- 4Byte;
+	double* - 8Byte;
+	--------------------
+	*/
+
+	for (int i = 0; i < n; i++)
+	{
+		cout << *(arr + i) << "\t";
+	}
+	cout << endl;
 }
