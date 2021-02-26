86bf40eb9bb4be6b07e18b27486740df7f69a3d0
Clayman <Clayman@DESKTOP-DM8E77U>
2021-02-26 17:06:47 +0200

    CStrings day 2

--- CStrings/CStrings/main.cpp
+++ CStrings/CStrings/main.cpp
@@ -2,7 +2,19 @@
 #include<Windows.h>
 using namespace std;
 
+#define tab "\t"
+
 int StrLen(char str[]);
+void ASCII()
+{
+	for (int i = 0; i < 256; i++)
+	{
+		cout << i << tab << (char)i << endl;
+	}
+}
+void to_upper(char str[]);
+void to_lower(char str[]);
+void capitalize(char str[]);
 
 void main()
 {
@@ -13,6 +25,7 @@ void main()
 	//char str[] = "Hello";
 	const int n = 20;
 	char str[n];
+	//ASCII();
 	cout << "Введите строку: ";
 	//SetConsoleCP(1251);
 	//cin >> str;
@@ -21,6 +34,10 @@ void main()
 	cout << str << endl;
 	cout << "Размер строки в Байтах:  " << sizeof(str) << endl;
 	cout << "Размер строки в символах:" << StrLen(str) << endl;
+	to_upper(str);
+	cout << str << endl;
+	capitalize(str);
+	cout << str << endl;
 }
 
 int StrLen(char str[])
@@ -28,4 +45,40 @@ int StrLen(char str[])
 	int i = 0;
 	for (; str[i]; i++);
 	return i;
+}
+
+void to_upper(char str[])
+{
+	for (int i = 0; str[i]; i++)
+	{
+		if (str[i] >= 'a' && str[i] <= 'z' || str[i] >= 'а' && str[i] <= 'я')
+		{
+			str[i] -= 32;
+		}
+		else if (str[i] == 'ё')str[i] -= 16;
+	}
+}
+
+void to_lower(char str[])
+{
+	for (int i = 0; str[i]; i++)
+	{
+		if (str[i] >= 'A' && str[i] <= 'Z' || str[i] >= 'А' && str[i] <= 'Я')
+		{
+			str[i] += 32;
+		}
+		else if (str[i] == 'Ё')str[i] += 16;
+	}
+}
+
+void capitalize(char str[])
+{
+	to_lower(str);
+	if (str[0] >= 'a'&&str[0] <= 'z' || str[0] >= 'а' && str[0] <= 'я')str[0] -= 32;
+	else if (str[0] == 'ё')str[0] -= 16;
+	for (int i = 1; str[i]; i++)
+	{
+		if ((str[i] >= 'a'&&str[i] <= 'z' || str[i] >= 'а' && str[i] <= 'я') && str[i - 1] == ' ')str[i] -= 32;
+		else if (str[i] == 'ё' && str[i - 1] == ' ')str[i] -= 16;
+	}
 }
