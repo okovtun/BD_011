d7516875494a57268f2d35e7016c0d9444747348
Clayman <Clayman@DESKTOP-DM8E77U>
2021-03-01 16:52:47 +0200

    CStrings day 2

--- CStrings/CStrings/main.cpp
+++ CStrings/CStrings/main.cpp
@@ -1,6 +1,9 @@
 #include<iostream>
 #include<Windows.h>
-using namespace std;
+//using namespace std;
+using std::cin;
+using std::cout;
+using std::endl;
 
 #define tab "\t"
 
@@ -15,6 +18,9 @@ void ASCII()
 void to_upper(char str[]);
 void to_lower(char str[]);
 void capitalize(char str[]);
+void shrink(char str[]);
+void remove_symbol(char str[], char symbol);
+bool is_palindrome(char str[]);
 
 void main()
 {
@@ -23,7 +29,7 @@ void main()
 	system("CLS");
 	//char str[] = { 'H', 'e', 'l', 'l', 'o', 0 };
 	//char str[] = "Hello";
-	const int n = 20;
+	const int n = 256;
 	char str[n];
 	//ASCII();
 	cout << "Введите строку: ";
@@ -37,6 +43,10 @@ void main()
 	to_upper(str);
 	cout << str << endl;
 	capitalize(str);
+	cout << str << endl;
+	shrink(str);
+	cout << str << endl;
+	cout << (is_palindrome(str) ? "Да" : "Нет") << endl;
 	cout << str << endl;
 }
 
@@ -81,4 +91,53 @@ void capitalize(char str[])
 		if ((str[i] >= 'a'&&str[i] <= 'z' || str[i] >= 'а' && str[i] <= 'я') && str[i - 1] == ' ')str[i] -= 32;
 		else if (str[i] == 'ё' && str[i - 1] == ' ')str[i] -= 16;
 	}
+}
+
+void shrink(char str[])
+{
+	for (int i = 0; str[i]; i++)
+	{
+		while (str[i] == ' ' && str[i + 1] == ' ')
+		{
+			for (int j = i; str[j]; j++)//Сдвигает часть строки влево
+			{
+				str[j] = str[j + 1];
+			}
+		}
+	}
+}
+
+void remove_symbol(char str[], char symbol)
+{
+	for (int i = 0; str[i]; i++)
+	{
+		while (str[i] == symbol)
+		{
+			for (int j = i; str[j]; j++)str[j] = str[j + 1];
+		}
+	}
+}
+
+bool is_palindrome(char str[])
+{
+	//Определяем длину строки
+	int size = StrLen(str);
+	char* buffer = new char[size + 1]{};
+	//for (int i = 0; str[i]; i++)buffer[i] = str[i];
+	strcpy(buffer, str);//String Copy. 
+						//buffer - строка получатель, 
+						//str - строка источник
+	to_lower(buffer);
+	remove_symbol(buffer, ' ');
+	size = strlen(buffer);
+	for (int i = 0; i < size / 2; i++)
+	{
+		if (buffer[i] != buffer[size - 1 - i])
+		{
+			delete[] buffer;
+			return false;
+		}
+	}
+	delete[] buffer;
+	return true;
 }
